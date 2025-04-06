18ff07a76d5063fd19ceacca311a6a6dd9d0f4b9
Kedlub <jakubkyzr@gmail.com>
2025-04-06 18:30:52 +0200

    feat: add sonner for toast notifications and update layout to include Toaster component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import AuthProvider from '@/providers/AuthProvider';
+import { Toaster } from '@/components/ui/sonner';
 
 export const metadata: Metadata = {
 	title: 'ActiveLife Sports Center',
@@ -16,6 +17,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={`antialiased`}>
 				<AuthProvider>{children}</AuthProvider>
+				<Toaster />
 			</body>
 		</html>
 	);
